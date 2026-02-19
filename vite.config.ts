817d73ebef4f5b507c4bcfaba41aeb504a50c27f
Laxmi Pandey <pandeylax30@gmail.com>
2026-02-19 15:38:48 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: '/hansa-legal-association/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
